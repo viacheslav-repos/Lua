0a98355af436280ea7c7830b1595f14fe29a268d
Slavik <viacheslav.mail@gmail.com>
2019-02-11 14:37:12 +0200

    Update search_in_tree_keys.lua

--- search_in_tree_keys.lua
+++ search_in_tree_keys.lua
@@ -1,52 +1,86 @@
+local tempTree
+local prevPattern
+
+local urlPartIndex = 1
+local urlParts = {}
+
+local resultPathParts = {}
+
 local tree = {
-  ["v1"] = {
-    ["webtexts"] = {
-      ["[a-zA-Z0-9_-]+"] = {
-        ["test"] = {
-          "finish1"
+    ["v1"] = {
+        ["users"] = {
+            ["[0-1]+"] = {
+                "1"
+            },
+            ["[2-3]+"] = {
+                "2"
+            },
+            ["[4-9]+"] = {
+                "3"
+            }
         },
-        ["[a-zA-Z0-9_-]+"] = {
-          "finish2"
+        ["[a-zA-Z0-9_]+"] = {
+            ["test"] = {
+                "4"
+            }
         }
-      },
-      ["product"] = {
-        ["test1"] = {
-          "finish3"
-        },
-        ["[a-zA-Z0-9_-]+"] = {
-          "finish4"
-        },
-        ["test2"] = {
-          "finish5"
-        }
-      }
     }
-  }
 }
 
-local function getRoutePattern(urlPart)
-  local hit
+local function getRoutePattern()
+    local hit
+    local firstKey
+    local urlPart = urlParts[urlPartIndex]
 
-  if tree[urlPart] then
-    hit = urlPart
-  else
-    for urlPattern in pairs(tree) do
-      if urlPart:match("^" .. urlPattern .. "$") then
-        hit = urlPattern
-        break
-      end
+    if tree[urlPart] then
+        hit = urlPart
+        firstKey = urlPart
+    else
+        for urlPattern in pairs(tree) do
+            if not firstKey then
+                firstKey = urlPattern
+            end
+
+            if urlPart:match("^" .. urlPattern .. "$") then
+                hit = urlPattern
+                break
+            end
+        end
     end
-  end
 
-  if hit then
-     tree = tree[hit]
+    if hit then
+        tempTree = tree
+        prevPattern = firstKey
+        urlPartIndex = urlPartIndex + 1
 
-     print(hit)
-  end
+        tree = tree[hit]
+
+        table.insert(resultPathParts, hit)
+    else
+        --- get previous level of tree, get previous url part, remove useless tree node and try again
+        tree = tempTree
+        tree[prevPattern] = nil
+        urlPartIndex = urlPartIndex - 1
+
+        table.remove(resultPathParts)
+    end
+
+    if urlParts[urlPartIndex] and tree then
+        getRoutePattern()
+    end
 end
 
-local url = "v1/webtexts/product/test1"
+function Core:access()
+    local url = "v1/users"
+    local found
 
-for urlPart in url:gmatch("([^/]+)") do
-  getRoutePattern(urlPart)
+    for urlPartLocal in url:gmatch("([^/]+)") do
+        table.insert(urlParts, urlPartLocal)
+    end
+
+    getRoutePattern()
+
+    ngx.log(1, Json.encode(resultPathParts))
+
+    return ngx.exit(ResponseCode.OK)
 end
